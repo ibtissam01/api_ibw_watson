df3cae9337bfe9c159bdb1bd1d707c772646f113
Ibtissam Labyady <89752387+ibtissam01@users.noreply.github.com>
2023-12-25 12:08:04 +0100

    Update main.py

--- main.py
+++ main.py
@@ -4,20 +4,17 @@ from ibm_watson import NaturalLanguageUnderstandingV1
 from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 from ibm_watson.natural_language_understanding_v1 \
     import Features, CategoriesOptions
+import streamlit as st
+from ibm_watson import NaturalLanguageUnderstandingV1
+from ibm_cloud_sdk_core.authenticators import IAMAuthenticator
 
 # Set up Watson NLU credentials
-apikey="Y5hX-s8UJ8PYMSZ40D5AGWqmeyE2T5zLDMXrlf7E4uHc"
-url="https://api.au-syd.natural-language-understanding.watson.cloud.ibm.com/instances/7fdb634c-d235-4d63-bfbb-0a928698fb41"
-
+apikey = "YOUR_API_KEY"
+url = "YOUR_SERVICE_URL"
 
 authenticator = IAMAuthenticator(apikey)
-natural_language_understanding = NaturalLanguageUnderstandingV1(
-    version='2022-04-07',
-    authenticator=authenticator
-)
-
-natural_language_understanding.set_service_url(url)
-
+nlu = NaturalLanguageUnderstandingV1(version='2021-09-01', authenticator=authenticator)
+nlu.set_service_url(url)
 
 # Streamlit app
 def main():
@@ -28,8 +25,11 @@ def main():
         if text_input:
             st.write("Analyzing...")
             features = {
-                "sentiment": {}
-        
+                "sentiment": {},
+                "categories": {},
+                "concepts": {},
+                "entities": {},
+                "keywords": {}
             }
             response = nlu.analyze(text=text_input, features=features).get_result()
 
@@ -38,7 +38,25 @@ def main():
             sentiment = response["sentiment"]["document"]["label"]
             st.write(f"Sentiment: {sentiment}")
 
-       
+            st.subheader("Categories")
+            categories = response["categories"]
+            for category in categories:
+                label = category["label"]
+                st.write(f"- {label}")
+
+            st.subheader("Entities")
+            entities = response["entities"]
+            for entity in entities:
+                name = entity["text"]
+                entity_type = entity["type"]
+                st.write(f"- {name} ({entity_type})")
+
+            st.subheader("Keywords")
+            keywords = response["keywords"]
+            for keyword in keywords:
+                text = keyword["text"]
+                relevance = keyword["relevance"]
+                st.write(f"- {text} (Relevance: {relevance})")
 
         else:
             st.warning("Please input some text to analyze.")
